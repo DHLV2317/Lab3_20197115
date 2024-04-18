3e2d9550fbaf98b022fe735877a365acf054745c
DHLV2317 <dhlopez@pucp.edu.pe>
2024-04-17 20:22:48 -0500

    Avance

---
+++ LAB3_20197115/src/main/java/com/example/lab3_20197115/Entity/Sede.java
@@ -0,0 +1,9 @@
+package com.example.lab3_20197115.Entity;
+
+import com.fasterxml.jackson.annotation.JsonTypeId;
+
+public class Sede {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="idsede)
+}
