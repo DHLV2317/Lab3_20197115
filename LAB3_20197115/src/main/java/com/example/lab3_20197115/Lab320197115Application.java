50baa55adae59efa37dc49eb0c6927391d3357a7
DHLV2317 <dhlopez@pucp.edu.pe>
2024-04-17 19:55:50 -0500

    Base de datos

---
+++ LAB3_20197115/src/main/java/com/example/lab3_20197115/Lab320197115Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20197115;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320197115Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320197115Application.class, args);
+    }
+
+}
